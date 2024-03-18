32f8cb477f7da14d0b297ead8fa42172cdeddbcd
ArnasPip <pipinisarnas@gmail.com>
2024-03-18 20:35:55 +0200

    minor fix, letters too similar on code verification

--- KTPS.Model/Helpers/RandomString.cs
+++ KTPS.Model/Helpers/RandomString.cs
@@ -8,7 +8,7 @@ public static class RandomString
     public static string GenerateRandomString()
     {
         var magickNumber = 6;
-        const string chars = "ABCDEFLEAGUEOFLEGENDSGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        const string chars = "ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz23456789";
         Random random = new Random();
         return new string(Enumerable.Repeat(chars, magickNumber).Select(s => s[random.Next(s.Length)]).ToArray());
     }
